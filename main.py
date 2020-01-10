b07697cb5d0010dbcaf3cba8c0a511241aa2090f
armandspucs <53978384+armandspucs@users.noreply.github.com>
2020-01-10 12:17:43 +0200

    a

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask('app')
@@ -6,9 +6,19 @@ app = Flask('app')
 
 @app.route('/')
 def index_page():
-  return "Sveiciens no StartIT! 4"
+  #return "Sveiciens no StartIT! 7"
+  return render_template('index.html')
+
+
+
+
+@app.route('/health') #what to return
+def health_check():
+  return 'OK'
+
+
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
