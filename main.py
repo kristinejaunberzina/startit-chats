744893cb001959af23aa62313fdbfc39aad6ab7d
armandspucs <53978384+armandspucs@users.noreply.github.com>
2020-04-30 00:24:19 +0300

    a

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
+app = Flask('app')
+
+
+@app.route('/')
+def index_page():
+  return "Sveiciens no StartIT! 7"
+
+
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000, debug=True)
