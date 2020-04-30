68532ee0e0069700dc4b9380b8b238101ad11cb8
armandspucs <53978384+armandspucs@users.noreply.github.com>
2020-04-30 09:05:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 
+class test(db.Model):
+  col = db.column(db.String(255), primary_key=True)
+  col2 = db.column(db.String(255), unique=True, nullable=False)
+
+  def __repr__(self):
+    return '%r' % self.col
+
+
 @app.route('/')
 def index_page():
   return "Sveiciens no StartIT! 7"
