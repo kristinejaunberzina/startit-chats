a80626fd793687eedb771cdf786e23f10c800040
armandspucs <53978384+armandspucs@users.noreply.github.com>
2020-04-30 08:38:19 +0300

    A

--- main.py
+++ main.py
@@ -1,14 +1,45 @@
 import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, json, jsonify, request
 
 
 app = Flask('app')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+
 
 @app.route('/')
 def index_page():
-  return "Sveiciens no StartIT! 7"
+  #return "Sveiciens no StartIT! 7"
+  return render_template('chats.html')
+
+
+
+
+@app.route('/health')
+def health_check():
+  return 'OK'
+
+
+@app.route('/chats/lasi')
+def ielasit_chatu():
+  chata_rindas = []
+  with open("chats.txt", 'r', encoding="UTF-8") as f:
+    for rinda in f:
+      chata_rindas.append(rinda)
+  return jsonify({"chats":chata_rindas})
+
+
+
+
+
+@app.route('/chats/suuti', methods = ['POST'])
+def suti_zinu():
+  dati = request.json;
+
+  with open("chats.txt", 'a', newline="", encoding="UTF-8") as f:
+    f.write(dati["chats"]+"\n")
+
+  return ielasit_chatu()
+
+
 
 
 
